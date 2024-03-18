4e82290dcf282cda259d6c656d9a4ba9b74de919
Danielsilva200319 <89894235+Danielsilva200319@users.noreply.github.com.>
2024-03-18 18:37:09 -0500

    Adding the introduction

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("This is the first line");
+        Console.WriteLine("This is the second line");
+    }
+}
